160a7945192df6010c9caffaa4532815a707582d
MMbah <MMbah@GTEKMOBILE113>
2020-02-12 15:12:55 -0500

    code commit

---
+++ demo/Controllers/EmployeesController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using demo.Models;
+using demo.Repository;
+
+namespace demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IDataRepository<Employee> _dataRepository;
+
+        public EmployeesController(IDataRepository<Employee> dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+
+        // GET: api/Employees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            IEnumerable<Employee> employees = _dataRepository.GetAll();
+            return Ok(employees);
+        }
+
+        // GET: api/Employees/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<ActionResult<Employee>> GetEmployee(long id)
+        {
+            var employee =  _dataRepository.Get(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return employee;
+        }
+
+        // PUT: api/Employees/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(long id, [FromBody]Employee employee)
+        {
+            if (employee == null)
+            {
+                return BadRequest("Oops employee is null");
+            }
+
+            Employee employeeToUpdate = _dataRepository.Get(id);
+            if(employeeToUpdate == null)
+            {
+                return NotFound("The Emplyee record could not be found");
+            }
+            _dataRepository.Update(employeeToUpdate, employee);
+            return NoContent();
+        }
+
+        // POST: api/Employees
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            if(employee == null)
+            {
+                return BadRequest("Employee is null");
+            }
+            _dataRepository.Add(employee);
+            return CreatedAtRoute("Get", new { Id = employee.EmployeeId }, employee);
+        }
+
+        // DELETE: api/Employees/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Employee>> DeleteEmployee(long id)
+        {
+            Employee employee = _dataRepository.Get(id);
+            if (employee == null)
+            {
+                return NotFound("The Employee record couldn't be found.");
+            }
+            _dataRepository.Delete(employee);
+            return NoContent();
+        }
+    }
+}
---
+++ demo/DataManager/EmployeeManager.cs
@@ -0,0 +1,52 @@
+﻿using demo.Data;
+using demo.Models;
+using demo.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demo.DataManager
+{
+    public class EmployeeManager : IDataRepository<Employee>
+    {
+
+        readonly EmployeeContext _employeeContext;
+        public EmployeeManager(EmployeeContext employeeContext)
+        {
+            _employeeContext = employeeContext;
+        }
+
+        public Employee Get(long id)
+        {
+            return _employeeContext.Employees.FirstOrDefault(e => e.EmployeeId == id);
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _employeeContext.Employees.ToList();
+        }
+        public void Add(Employee entity)
+        {
+            _employeeContext.Employees.Add(entity);
+            _employeeContext.SaveChanges();
+        }
+
+        public void Delete(Employee entity)
+        {
+            _employeeContext.Employees.Remove(entity);
+            _employeeContext.SaveChanges();
+        }
+
+        public void Update(Employee dbEntity, Employee entity)
+        {
+            dbEntity.FirstName = entity.FirstName;
+            dbEntity.LastName = entity.LastName;
+            dbEntity.Email = entity.Email;
+            dbEntity.DateOfBirth = entity.DateOfBirth;
+            dbEntity.PhoneNumber = entity.PhoneNumber;
+
+            _employeeContext.SaveChanges();
+        }
+    }
+}
